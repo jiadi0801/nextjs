f8d21da3cf5763fa2df808397f15969981633223
Liu Jia <ldspirit0801@163.com>
2020-09-03 11:49:01 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>liujia.ldspirit &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
